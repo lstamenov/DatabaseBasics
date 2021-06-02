838593cbd5a1ff8ccb6af52df8ac0cf802bb184b
lstamenov <stamenovl99@gmail.com>
2021-06-03 01:11:55 +0300

    Added the rest pf the tasks from joins and subqueries

--- JoinsAndSubqueriesExercise.sql
+++ JoinsAndSubqueriesExercise.sql
@@ -122,3 +122,35 @@ ON c.`country_code` = mc.`country_code`
 WHERE c.`country_code` IN ('BG', 'RU', 'US')
 GROUP BY c.`country_code`
 ORDER BY `mountain_range` DESC;
+
+#14. Task
+SELECT c.`country_name`, r.`river_name` 
+FROM `countries` AS c
+LEFT JOIN `countries_rivers` AS cr
+ON c.`country_code` = cr.`country_code`
+LEFT JOIN `rivers` AS r
+ON cr.`river_id` = r.`id`
+WHERE c.`continent_code` LIKE 'AF'
+ORDER BY c.`country_name`
+LIMIT 5;
+
+#17. Task
+SELECT c.`country_name`, 
+MAX(p.`elevation`) AS `highest_peak_elevation`,
+MAX(r.`length`) AS `longest_river_length`
+FROM `countries` AS c
+JOIN `mountains_countries` AS mc
+ON c.`country_code` = mc.`country_code`
+JOIN `mountains` AS m
+ON mc.`mountain_id` = m.`id`
+JOIN `peaks` AS p
+ON m.`id` = p.`mountain_id`
+JOIN `countries_rivers` AS cr
+ON c.`country_code` = cr.`country_code`	
+JOIN `rivers` AS r
+ON cr.`river_id` = r.`id`
+GROUP BY c.`country_code`
+ORDER BY `highest_peak_elevation` DESC, 
+`longest_river_length` DESC, c.`country_name`
+LIMIT 5;
+
