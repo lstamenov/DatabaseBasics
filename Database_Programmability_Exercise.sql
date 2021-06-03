7254be7291021ed2da1bf223c1514d74a599a00d
lstamenov <stamenovl99@gmail.com>
2021-06-03 22:44:18 +0300

    Added tasks 5-9

--- Database_Programmability_Exercise.sql
+++ Database_Programmability_Exercise.sql
@@ -74,9 +74,58 @@ END//
 CALL usp_get_employees_salary_level();
 
 #6. Task
+DELIMITER //
+CREATE PROCEDURE 
+usp_get_employees_by_salary_level(salary_level VARCHAR(10))
+BEGIN
+SELECT first_name, last_name FROM employees
+WHERE ufn_get_salary_level(salary) LIKE salary_level
+ORDER BY first_name DESC, last_name DESC;
+END//
+
+CALL usp_get_employees_by_salary_level('High');
+
 #7. Task
+DELIMITER //
+CREATE FUNCTION ufn_is_word_comprised(set_of_chars VARCHAR(50),
+word VARCHAR(50))
+RETURNS BIT
+BEGIN
+	RETURN word REGEXP (concat('^[', set_of_chars, ']+$'));
+END//
+
 #8. Task
+DELIMITER //
+CREATE PROCEDURE usp_get_holders_name()
+BEGIN
+	SELECT CONCAT(first_name ,' ', last_name) AS full_name
+    FROM account_holders
+    ORDER BY full_name, id;
+END//
+
+CALL usp_get_holders_name();
+
 #9. Task
+DELIMITER//
+CREATE PROCEDURE 
+usp_get_holders_with_balance_higher_than(min_balance DOUBLE)
+BEGIN
+    SELECT 
+         h.first_name, h.last_name
+    FROM
+        `account_holders` AS h
+            JOIN
+        (SELECT 
+            a.id, a.account_holder_id, SUM(a.balance) AS 'total_balance'
+        FROM
+            `accounts` AS a
+        GROUP BY a.account_holder_id
+        HAVING `total_balance` > min_balance) as a ON h.id = a.account_holder_id
+    ORDER BY a.id;
+END//
+
+CALL usp_get_holders_with_balance_higher_than(7000);
+
 #10. Task
 #11. Task
 #12. Task
